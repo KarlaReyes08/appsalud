b8afd3c38f31203b62d98b71fc6419112ec5a9c0
KarlaReyes08 <ayuminadoka102@gmail.com>
2024-07-27 22:34:19 -0600

    commit

---
+++ app/src/main/java/com/entrenamiento/appsalud/MainActivity.java
@@ -0,0 +1,47 @@
+package com.entrenamiento.appsalud;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.entrenamiento.appsalud.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new InicioFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener  (item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new InicioFragment());
+            } else if (id == R.id.info) {
+                replaceFragment(new InfoFragment());
+            } else if (id == R.id.contact) {
+                replaceFragment(new ContactoFragment());
+            }
+            return true;
+        });
+        }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
